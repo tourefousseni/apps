56290a57b98a006ef49bbbf89a93826a805cdc9f
foussenitoure <tourefousseni1@gmail.com>
2021-02-04 20:03:38 +0000

    update files base.html

--- static/js/jquery.js
+++ static/js/jquery.js
@@ -1,1 +1,8 @@
 alert("La page a été actualisée avec success !");
+
+$(document).ready(function(){
+$("#status").on('click', function () {
+  var getValue= $("#personne").val();
+   alert(getValue);
+ });
+});

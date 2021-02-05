7e589bc25ad8c41c1c6899d8510c6ac4990d2b7f
foussenitoure <tourefousseni1@gmail.com>
2021-02-05 23:10:22 +0000

    update for files base.html and delete staticfiles

--- pyblog/settings.py
+++ pyblog/settings.py
@@ -118,5 +118,5 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)                    ]
--- staticfiles/js/jquery.js
+++
@@ -1,1 +0,0 @@
-alert("Bonjour les douniya soba!");

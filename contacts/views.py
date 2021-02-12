e5ed8a877c85d2ba82af340f066c83b131865310
foussenitoure <tourefousseni1@gmail.com>
2021-02-12 23:47:39 +0000

    update for files projects

--- contacts/views.py
+++ contacts/views.py
@@ -6,6 +6,88 @@ from contacts.models import Contact
 from contacts.form import ContactForm
 # from .forms import UploadFileForm
 
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.http import Http404
+from .import views
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm, PasswordChangeForm
+from django.contrib.auth import authenticate, login, logout, update_session_auth_hash
+from django.contrib import messages
+from .forms import SignUpForm, EditProfileForm
+
+
+def home(request):
+    return render(request, 'authenticate/home.html', {})
+
+def about(request):
+    return render(request, 'authenticate/about.html', {})
+
+def user_login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request,('You Have been Logged In !'))
+            return redirect('home')
+        else:
+            messages.success(request, ('Error you can try again !'))
+            return redirect('login')
+    else:
+        return render(request, 'authenticate/login.html', {})
+
+def logout_user(request):
+     logout(request)
+     messages.success(request, ('You Have Been Logged out...'))
+     return redirect('home')
+
+def register_user(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            messages.success(request,('You Have Registered now...'))
+            return redirect('home')
+
+    else:
+        form = SignUpForm(request.POST)
+    context = {'form': form}
+    return render(request, 'authenticate/register.html', context)
+
+
+def edit_profile(request):
+    if request.method == 'POST':
+        form = EditProfileForm(request.POST, instance=request.user)
+        if form.is_valid():
+            form.save()
+            messages.success(request,('You Have Edited Your Profiel...'))
+            return redirect('home')
+
+    else:
+        form = EditProfileForm(instance=request.user)
+    context = {'form': form}
+    return render(request, 'authenticate/edit_profile.html', context)
+
+
+def change_password(request):
+    if request.method == 'POST':
+        form = PasswordChangeForm(data=request.POST, user=request.user)
+        if form.is_valid():
+            form.save()
+            update_session_auth_hash(request, form.user)
+            messages.success(request, ('You Have Edited Your Password...'))
+            return redirect('home')
+
+    else:
+        form = PasswordChangeForm(user=request.user)
+    context = {'form': form}
+
+    return render(request, 'authenticate/change_password.html', context)
 
 
 
@@ -69,3 +151,10 @@ def contact_detail(request, contact_id):
     }
         return render(request, 'contacts/contacts_detail.html', context)
 
+
+def profil(request,):
+    ps = Contact.objects.all()
+    context = {
+        'contacts': ps,
+    }
+    return render(request, 'contacts/profil.html', context)
--- pyblog/urls.py
+++ pyblog/urls.py
@@ -8,6 +8,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 
+
 app_name = 'blog', \
            'contacts',
 # app_name = 'contacts',
@@ -18,8 +19,27 @@ urlpatterns = [
       # path('posts/<str:category_name>/', views.post_list, name='post-list'),
       # path('posts/detail/<int:post_id>/', views.post_detail, name='post-detail'),
       path('thanks/', views.thanks, name='thanks'),
+      path('profil/', views.profil, name='profil'),
       path('contact/', views.contact, name='adresses'),
       path('contacts/detail/<int:contact_id>/', views.contact_detail, name='contact-detail'),
+
+
 ]
  # + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 # post_list/
+
+
+#
+# urlpatterns = [
+#      url(r'^admin/', admin.site.urls),
+#      # url(r'', admin.site.urls),
+#      url(r'^home/$', views.home, name='home'),
+#      # url(r'^admin/$', views.home, name='home'),
+#      url(r'^about/$', views.about, name='about'),
+#      url(r'^login/$', views.user_login, name='login'),
+#      url(r'^logout/$', views.logout_user, name='logout'),
+#      url(r'^register/$', views.register_user, name='register'),
+#      url(r'^edit/profile/$', views.edit_profile, name='edit_profile'),
+#      url(r'^change/password/$', views.change_password, name='change_password'),
+#      url(r'^products/', include('products.urls', namespace='products')),
+# ]

3fe21d0e58c091b5a6b4aa03b5fa51a566dad864
toure <tourefousseni1@gmail.com>
2021-11-17 00:34:39 -0800

    off leaflet now

--- pyblog/settings.py
+++ pyblog/settings.py
@@ -28,7 +28,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     # Our the app
     'django.contrib.gis',
-    'leaflet',
+    # 'leaflet',
     'contacts',
     # 'bootstrap',
     'django_bootstrap_datetimepicker',

65e22405b1fbf91b6cc3d7a5c1037da447e41b02
toure <tourefousseni1@gmail.com>
2022-02-01 03:50:07 +0000

    update image views on branch kala

--- pyblog/settings.py
+++ pyblog/settings.py
@@ -109,7 +109,7 @@ USE_TZ = True
 
 STATIC_URL       = '/static/'
 MEDIA_URL        = '/media/'
-MEDIA_ROOT       = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT       = os.path.join(BASE_DIR, 'media/')
 STATIC_ROOT      = os.path.join(BASE_DIR, 'static_root')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'pyblog/static')]
 

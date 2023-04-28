e19a8969c07d1b0bcafc8018377ef752bdd84b78
foussenitoure <tourefousseni1@gmail.com>
2023-04-28 02:15:02 +0000

    update again 13 changed icons list person

--- accounts/admin.py
+++ accounts/admin.py
@@ -12,6 +12,8 @@ class AccountsAdmin(admin.ModelAdmin):
                     'phone',
                     "date_joined",)
     exclude = ('code', 'img', )
-
+    list_filter = ("is_staff", "is_active",)
+    search_fields = ("last_name__startswith",)
+admin.site.site_header = 'Admininstration kalaliso'
 admin.site.register(User, AccountsAdmin)
 
